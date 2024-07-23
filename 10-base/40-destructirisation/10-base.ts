6d191a12c3b02125c0b1bba9a3efc85977347de9
A.Zahliada <edu81zau@gmail.com>
2024-07-23 14:00:34 +0300

    Add function-wraper

--- 10-base/40-destructirisation/10-base.ts
+++ 10-base/40-destructirisation/10-base.ts
@@ -1,42 +1,48 @@
-//const isBirthdayDate: boolean = true;
-//let ageDate: number = 40;
-//const userNameDate: string = "John";
+(() => {
+  //const isBirthdayDate: boolean = true;
+  //let ageDate: number = 40;
+  //const userNameDate: string = "John";
 
-//let salary: number;
-//salary = 5000;
+  //let salary: number;
+  //salary = 5000;
 
-const userData =
-  '{"isBirthdayDate": true, "ageDate": 40, "userNameDate": "John"}';
+  const userData =
+    '{"isBirthdayDate": true, "ageDate": 40, "userNameDate": "John"}';
 
-const userObj: {
-  isBirthdayDate: boolean;
-  userNameDate: string;
-  ageDate: number;
-} = JSON.parse(userData);
+  const userObj: {
+    isBirthdayDate: boolean;
+    userNameDate: string;
+    ageDate: number;
+  } = JSON.parse(userData);
 
-console.log(userObj);
-console.log(
-  "------------------------------------------------------------------"
-);
+  console.log(userObj);
+  console.log(
+    "------------------------------------------------------------------"
+  );
 
-function logBrMsg(isBirthday: boolean, userName: string, age: number): string {
-  console.log("logBrMsg");
-  if (isBirthday) {
-    return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
-    console.log(
-      "------------------------------------------------------------------"
-    );
-  } else {
-    return "Error";
-    console.log(
-      "------------------------------------------------------------------"
-    );
+  function logBrMsg(
+    isBirthday: boolean,
+    userName: string,
+    age: number
+  ): string {
+    console.log("logBrMsg");
+    if (isBirthday) {
+      return `Congrats ${userName.toUpperCase()}, age: ${age + 1}`;
+      console.log(
+        "------------------------------------------------------------------"
+      );
+    } else {
+      return "Error";
+      console.log(
+        "------------------------------------------------------------------"
+      );
+    }
   }
-}
 
-let res = logBrMsg(
-  userObj.isBirthdayDate,
-  userObj.userNameDate,
-  userObj.ageDate
-);
-console.log("res=", res);
+  let res = logBrMsg(
+    userObj.isBirthdayDate,
+    userObj.userNameDate,
+    userObj.ageDate
+  );
+  console.log("res=", res);
+})();
