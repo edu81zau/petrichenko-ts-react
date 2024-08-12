3dca696ec95ad82a71f520c98ec1e45839a416be
A.Zahliada <edu81zau@gmail.com>
2024-08-12 12:56:46 +0300

    Continue to doing work of DOM

---
+++ 20-necessary-level/150-lesson-practic/sander.js
@@ -0,0 +1,5 @@
+console.log("Test 1", document.getElementById("email")); //поиск по id при помощи ID
+console.log("Test 2", document.querySelector("#email")); //поиск по id при помощи селектора
+console.log("Test 3", document.querySelector('* [id="emailHelp"]')); //поиск по id при помощи селектора
+console.log("Test 4", document.querySelectorAll("input")); //поиск по id при помощи селектора
+console.log("Test 4", document.querySelectorAll('input[type="checkbox"]')); //поиск по id при помощи селектора
