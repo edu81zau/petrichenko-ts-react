2cad5cde5d3e801a75d02c038481dd1a707723fa
A.Zahliada <edu81zau@gmail.com>
2024-08-14 14:26:29 +0300

    doing practic lesson of DOM

--- 20-necessary-level/150-lesson-practic/index.ts
+++ 20-necessary-level/150-lesson-practic/index.ts
@@ -1,49 +1,64 @@
-(() => {
-  const forms = document.querySelectorAll("form");
-  const email = document.querySelector("#email") as HTMLInputElement;
-  const title = document.querySelector("#title") as HTMLInputElement;
-  const text = document.querySelector("#text") as HTMLTextAreaElement;
-  const checkbox = document.querySelector("#checkbox") as HTMLInputElement;
+//(() => {
+const forms = document.querySelectorAll("form");
+const email = document.querySelector("#email") as HTMLInputElement;
+const title = document.querySelector("#title") as HTMLInputElement;
+const text = document.querySelector("#text") as HTMLTextAreaElement;
+const checkbox = document.querySelector("#checkbox") as HTMLInputElement;
 
-  interface IFormData {
-    email: string;
-    title: string;
-    text: string;
-    checkbox: boolean;
-  }
+interface IFormData {
+  email: string;
+  title: string;
+  text: string;
+  checkbox: boolean;
+}
 
-  const formData: IFormData = {
-    email: "",
-    title: "",
-    text: "",
-    checkbox: false,
-  };
+const formData: IFormData = {
+  email: "",
+  title: "",
+  text: "",
+  checkbox: false,
+};
 
-  // Последовательность действий:
-  // 1) Происходит submit любой из форм
-  // 2) Все данные из 4х полей со страницы переходят в свойства объекта formData
-  // 3) Запускается функция validateFormData с этим объектом, возвращает true/false
-  // 4) Если на предыдущем этапе true, то запускается функция checkFormData с этим объектом
+forms.forEach((form) => {
+  console.log("form", typeof form);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  function validateFormData(data) {
-    // Если каждое из свойств объекта data правдиво...
-    if ("condition") {
-      return true;
-    } else {
-      console.log("Please, complete all fields");
-      return false;
+    formData.email = email?.value ?? "";
+    formData.title = title?.value ?? "";
+    formData.text = text?.value ?? "";
+    formData.checkbox = checkbox?.checked ?? false;
+
+    if (validateFormData(formData)) {
+      checkFormData(formData);
     }
-  }
+  });
+});
 
-  function checkFormData(data) {
-    const { email } = data;
-    const emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
-
-    // Если email совпадает хотя бы с одним из массива
-    if ("condition") {
-      console.log("This email is already exist");
-    } else {
-      console.log("Posting data...");
+function validateFormData(data: IFormData) {
+  const isValid = Object.values(data).every(
+    //
+    function (value) {
+      return value;
     }
+  );
+  // Если каждое из свойств объекта правдиво...
+  if (isValid) {
+    return true;
+  } else {
+    console.log("Please, complete all fields");
+    return false;
   }
-})();
+}
+
+function checkFormData(data: IFormData) {
+  const { email } = data;
+  const emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
+
+  if (emails.some((e) => e === email)) {
+    console.log("This email is already exist");
+  } else {
+    console.log("Posting data...");
+  }
+}
+//})();
--- 20-necessary-level/150-lesson-practic/sander.js
+++ 20-necessary-level/150-lesson-practic/sander.js
@@ -1,3 +1,8 @@
+/**
+ * @TODO: пройти лекцию про html form and input submit
+ * preventDefault() - search and read information
+ * разобрать последовательность Object.values(data).every(...)
+ */
 console.log("Test 1", document.getElementById("email")); //поиск по id при помощи ID
 console.log("Test 2", document.querySelector("#email")); //поиск по id при помощи селектора
 console.log("Test 3", document.querySelector('* [id="emailHelp"]')); //поиск по id при помощи селектора
